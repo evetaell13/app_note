6e2f6d98bbc03ce4c9106c85ed21eed0d6c984ca
Svetlana Khmeleva <evetaell13@yandex.ru>
2022-03-12 17:55:55 +0400

    Initial commit

---
+++ app_user_note/app_user_note/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from app_user_note.users.models import User
+from app_user_note.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
